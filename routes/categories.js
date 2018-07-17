2a71385645c7257d4372b0773c482222350c7448
Stefan Peters <stefandesu@exo.pm>
2018-07-17 20:57:57 +0200

    Add mensa parameter to categories endpoint

--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,11 @@ const collection = "categories"
 
 module.exports = function(app, db) {
   app.get("/categories", (req, res) => {
-    db.collection(collection).find()
+    let query = {}
+    if (req.query.mensa) {
+      query.mensa = req.query.mensa
+    }
+    db.collection(collection).find(query)
       .toArray()
       .then(results => {
         res.json(results)
